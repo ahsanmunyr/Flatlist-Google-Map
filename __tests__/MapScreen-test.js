e5dcb84ff11254140c6d8211fa866cf3171939d8
M Ahsan Muneer <kbahsan@ehmad11.com>
2023-05-13 18:33:08 +0500

    changes

--- __tests__/MapScreen-test.js
+++ __tests__/MapScreen-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Loader from '../src/components/Loader';
-import { venueGet } from '../src/store/actions/venueGet';
+
 test('renders loader component currectly', () => {
   const loaderView = renderer.create(<Loader />).toJSON();
   console.log(loaderView.props,  "loaderView");
